9d73f05ed2ea22115c618b344cde7e0c94c0e234
Steve Purcell <steve@sanityinc.com>
2013-09-07 09:41:42 +0100

    Use smie indentation from css-mode in latest Emacs HEAD

--- less-css-mode.el
+++ less-css-mode.el
@@ -185,6 +185,10 @@ Special commands:
   (set (make-local-variable 'comment-start) "//")
   (set (make-local-variable 'comment-end) "")
   (set (make-local-variable 'indent-line-function) 'less-css-indent-line)
+  (when (functionp 'css-smie-rules)
+    (smie-setup css-smie-grammar #'css-smie-rules
+                :forward-token #'css-smie--forward-token
+                :backward-token #'css-smie--backward-token))
 
   (add-hook 'after-save-hook 'less-css-compile-maybe nil t))
 
